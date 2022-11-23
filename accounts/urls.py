7247a52c62c95d10122a57bf15357d371e0bdde8
fernando-izar <fizar@me.com>
2022-11-23 17:13:44 -0300

    feat: testing ipdb debugger

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("accounts/<int:account_id>/", views.AccountView.as_view()),
+    path("accounts/", views.AccountCreateView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework.views import APIView, status
 from rest_framework.response import Response
 from django.forms.models import model_to_dict
 
 from .models import Account
+import ipdb
 
 
 class AccountView(APIView):
@@ -15,4 +16,14 @@ class AccountView(APIView):
 
         account_dict = model_to_dict(account)
 
+        ipdb.set_trace()
+
         return Response(account_dict)
+
+
+class AccountCreateView(APIView):
+    def post(self, request):
+        account = Account.objects.create(**request.data)
+        account_dict = model_to_dict(account)
+
+        return Response(account_dict, status.HTTP_201_CREATED)

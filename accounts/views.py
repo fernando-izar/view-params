83ebeb1194c3bd0981880be7c761d26ffa17f120
fernando-izar <fizar@me.com>
2022-11-23 16:56:58 -0300

    finish exercise

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.forms.models import model_to_dict
 
-# Create your views here.
+from .models import Account
+
+
+class AccountView(APIView):
+    def get(self, request, account_id):
+        try:
+            account = Account.objects.get(pk=account_id)
+        except Account.DoesNotExist:
+            return Response({"error": "account not found"}, 404)
+
+        account_dict = model_to_dict(account)
+
+        return Response(account_dict)
